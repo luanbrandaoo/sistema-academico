6c70f1423c27bc63e5c37f6f01141c865324d692
Rafael Moreira <rafamuller05@gmail.com>
2025-07-08 20:49:59 -0300

    Desenvolvimento da classe abstrata Disciplina, superclasse de DisciplinaEletiva, DisciplinaObrigatoria e DisciplinaOptativa

--- src/main/java/ufjf/poo/model/disciplina/Disciplina.java
+++ src/main/java/ufjf/poo/model/disciplina/Disciplina.java
@@ -1,4 +1,39 @@
 package ufjf.poo.model.disciplina;
 
-public class Disciplina {
+import java.util.LinkedList;
+
+public abstract class Disciplina {
+    protected String codigo;
+    protected String nome;
+    protected int cargaHoraria;
+    protected LinkedList<Disciplina> preRequisitos;
+    protected LinkedList<Disciplina> coRequisitos;
+
+    public Disciplina(String codigo, String nome, int cargaHoraria) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public abstract String getCodigo();
+    public abstract String getNome();
+    public abstract int getCargaHoraria();
+    public abstract LinkedList<Disciplina> getPreRequisitos();
+    public abstract LinkedList<Disciplina> getCoRequisitos();
+
+    public abstract void setCodigo(String codigo);
+    public abstract void setNome(String nome);
+    public abstract void setCargaHoraria(int cargaHoraria);
+    public abstract void setPreRequisitos(LinkedList<Disciplina> preRequisitos);
+    public abstract void setCoRequisitos(LinkedList<Disciplina> coRequisitos);
+
+    public abstract void addPreRequisito(Disciplina disciplina);
+    public abstract void addCoRequisito(Disciplina disciplina);
+
+    public abstract void removePreRequisito(Disciplina disciplina);
+    public abstract void removeCoRequisito(Disciplina disciplina);
+
+    public abstract int getCreditos();
+    public abstract tipoDisciplina getTipo();
+    // equiparação?
 }
