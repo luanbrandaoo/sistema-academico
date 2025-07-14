423c0c3f8eeb65d4aa061326c12081ef4d5694c7
Rafael Moreira <rafamuller05@gmail.com>
2025-07-13 23:30:34 -0300

    Formata DiaHorario

--- src/main/java/ufjf/poo/model/Turma.java
+++ src/main/java/ufjf/poo/model/Turma.java
@@ -3,6 +3,8 @@ package ufjf.poo.model;
 ID da turma, capacidade máxima, número atual de alunos matriculados e o horário fixo da aula.
  */
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
 import java.util.LinkedList;
 
 public class Turma {
@@ -42,6 +44,16 @@ public class Turma {
     }
     @Override
     public String toString() {
-        return "Turma{" + "id=" + id + ", capacidadeMaxima=" + capacidadeMaxima + ", numeroAlunosMatriculados=" + numeroAlunosMatriculados + ", horario=" + horarios + '}';
+        return "Turma{" + "id=" + id + ", capacidadeMaxima=" + capacidadeMaxima +
+                ", numeroAlunosMatriculados=" + numeroAlunosMatriculados +
+                ", horario=" + horarios + '}';
+    }
+    public LinkedList<DiaHorario> formataDiaHorario(LinkedList<DiaHorario> horarios, DayOfWeek dia, int hora) {
+        if (horarios == null)
+            horarios = new LinkedList<>();
+        final int minutos = 0;
+        DiaHorario novoHorario = new DiaHorario(dia, LocalTime.of(hora,minutos));
+        horarios.add(novoHorario);
+        return horarios;
     }
 }
