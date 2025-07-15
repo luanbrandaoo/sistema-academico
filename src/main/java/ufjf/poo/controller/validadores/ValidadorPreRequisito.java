0ebacd66f6b5221e042d318a2080dfc89416b87a
Rafael Moreira <rafamuller05@gmail.com>
2025-07-15 01:05:18 -0300

    Parametro não utilizado

--- src/main/java/ufjf/poo/controller/validadores/ValidadorPreRequisito.java
+++ src/main/java/ufjf/poo/controller/validadores/ValidadorPreRequisito.java
@@ -1,8 +1,7 @@
 package ufjf.poo.controller.validadores;
 
 import ufjf.poo.model.Aluno;
-import ufjf.poo.model.disciplina.Disciplina;
 
 public interface ValidadorPreRequisito {
-    boolean validar(Aluno aluno, Disciplina disciplina);
+    boolean validar(Aluno aluno);
 }
